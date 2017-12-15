261737790e4ae8bf7b853f0d881edda84372fbdd
Denys Havryliv <bivi325@gmail.com>
2017-12-15 02:59:59 +0200

    Added score

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var canvas, canvasContext;
 
 var ballX = 50, ballY = 20;
-var ballSpeedX = 200;
+var ballSpeedX = 150;
 var ballSpeedY = 200;
 const BALL_SIZE = 10;
 const BALL_COLOR = 'red';
 
 var paddle1Y = 250;
 var paddle2Y = 250;
+const paddleSpeed = 250;
 const PADDLE_OFFSET = 20;
 const PADDLE_HEIGHT = 100;
 const PADDLE_THICKNESS = 20;
@@ -15,6 +16,10 @@ const PADDLE_COLOR = 'white';
 
 const framesPerSecond = 30;
 const time = 1000 / framesPerSecond;
+const t = 1/framesPerSecond;
+
+var firstPlayerScore = 0;
+var secondplayerScore = 0;
 
 function calculateMausePos(evt) {
   var rect = canvas.getBoundingClientRect();
@@ -31,7 +36,7 @@ window.onload = function() {
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
   canvasContext.fillStyle = 'black';
-  canvasContext.fillRect(0,0,canvas.width, canvas.height);
+  canvasContext.fillRect(0,0,canvas.width, canvas.height);  
   setInterval(function() {
     drawCanvas();
     ballMovement();
@@ -46,9 +51,13 @@ window.onload = function() {
     paddle1Y = mousePos.y - PADDLE_HEIGHT/2;
   })
 }
-function drawCanvas() {
+function drawCanvas() {  
   canvasContext.fillStyle = 'black';
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  canvasContext.fillStyle = 'white';
+  canvasContext.font="34px Georgia";
+  canvasContext.fillText(firstPlayerScore, 100, 100);
+  canvasContext.fillText(secondplayerScore, canvas.width - 200, 100);
 }
 
 function drawRocket(leftX, topY, width, height, drawColor) {
@@ -64,6 +73,8 @@ function drawBall(centerX, centerY, radius, drawColor) {
 }
 
 function ballReset() {
+  ballSpeedX = 150;
+  ballSpeedY = 200;
   let random = (Math.random() * 2) - 1;
   if(random > 0){
     ballSpeedY *= Math.ceil(random);
@@ -73,46 +84,71 @@ function ballReset() {
     ballSpeedX *= Math.floor(random);
   }else {
     ballSpeedY = 200;
-    ballSpeedX = 200;
+    ballSpeedX = 150;
   }  
   ballX = canvas.width/2;
   ballY = canvas.height/2;
 }
 
-function ballSpeedChange() {
+function ballSpeedChange(paddle) {
   ballSpeedX = -ballSpeedX;
-  ballSpeedY = -ballSpeedY;
+  let deltaY;
+  if(paddle == 'first'){
+    deltaY = ballY - (paddle1Y + PADDLE_HEIGHT / 2)
+  }else if(paddle == 'second'){
+    deltaY = ballY - (paddle2Y + PADDLE_HEIGHT / 2)
+  }  
+  ballSpeedY = deltaY * 8;
 }
 
 function computerMovement() {
-  if((paddle2Y + PADDLE_HEIGHT/2) < ballY){
-    paddle2Y += Math.abs(ballSpeedY) * time / 1000;
-  }else if((paddle2Y + PADDLE_HEIGHT/2) > ballY) {
-    paddle2Y -= Math.abs(ballSpeedY) * time / 1000;
+  let paddle2YCenter = paddle2Y + PADDLE_HEIGHT/2;
+  let localSpeed = Math.abs(paddle2YCenter - ballY)/t;
+  let speed = localSpeed < paddleSpeed ? localSpeed: paddleSpeed;
+  console.log(speed);
+  if(paddle2YCenter < ballY + 35 && ballSpeedX > 0 && ballX > 600){
+      paddle2Y += speed * t;  
+  }else if(paddle2YCenter > ballY - 35 && ballSpeedX > 0 && ballX > 600) {
+      paddle2Y -= speed * t;
+  }else if(ballSpeedX < 0){
+    moveToTheCenter();
   }else{
-    paddle2Y += (Math.random() * 2) - 1;
+    // do nothing
+  }
+ } 
+
+function moveToTheCenter() {
+  let paddle2YCenter = paddle2Y + PADDLE_HEIGHT/2;
+  if(paddle2YCenter > canvas.height / 2 + 35){
+     paddle2Y -= paddleSpeed * t;
+  }else if(paddle2YCenter < canvas.height /2 - 35){
+     paddle2Y += paddleSpeed * t;
+  }else {
+    // do nothing
   }
 }
 
 function ballMovement() {
-  ballX = ballX + ballSpeedX * time / 1000;
-  ballY = ballY + ballSpeedY * time / 1000;
+  ballX = ballX + ballSpeedX * t;
+  ballY = ballY + ballSpeedY * t;
 
   if(ballX > PADDLE_OFFSET && ballX < PADDLE_OFFSET + PADDLE_THICKNESS){
     if(ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT){
-      ballSpeedChange();
+      ballSpeedChange('first');
     }
   }
   if(ballX < canvas.width - PADDLE_OFFSET && ballX > canvas.width - PADDLE_OFFSET - PADDLE_THICKNESS){
     if(ballY > paddle2Y && ballY < paddle2Y + PADDLE_HEIGHT){
-      ballSpeedChange();
+      ballSpeedChange('second');
     }
   }
   if(ballX >= canvas.width){
     ballReset();
+    firstPlayerScore++;
     // point for player
   }else if (ballX <= 0) {
     ballReset();
+    secondplayerScore++;
     // point for computer
   }
   if(ballY >= canvas.height || ballY <= 0) { ballSpeedY = -ballSpeedY;}
